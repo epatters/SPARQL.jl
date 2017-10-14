e987be51c69b7a81069d268bb9c00508d222b7fa
epatters <ejpatters@gmail.com>
2017-10-13 23:41:53 -0500

    WIP: First cut at AST types for the four SPARQL query statements.
    Namely: SELECT, CONSTRUCT, ASK, and DESCRIBE.

--- src/AST.jl
+++ src/AST.jl
@@ -1,6 +1,9 @@
 module AST
-export SPARQLNode, Statement, Node, Resource, ResourceURI, ResourceCURIE,
-  Literal, Blank, Variable, Triple
+export SPARQLNode, Clause, Statement,
+  Node, Resource, ResourceURI, ResourceCURIE, Literal, Blank, Variable, Triple,
+  Expression,
+  PrologueClause, BaseURI, Prefix,
+  Query, Select, Construct, Ask, Describe
 
 using AutoHashEquals
 
@@ -8,7 +11,11 @@ using AutoHashEquals
 """
 abstract type SPARQLNode end
 
-""" Base type for statement in SPARQL grammar.
+""" Base type for clauses (subordinate expressions) in SPARQL grammar.
+"""
+abstract type Clause <: SPARQLNode end
+
+""" Base type for statements (complete expressions) in SPARQL grammar.
 """
 abstract type Statement <: SPARQLNode end
 
@@ -50,5 +57,73 @@ end
 # Convenience constructors
 Resource(uri::String) = ResourceURI(uri)
 Resource(prefix::String, name::String) = ResourceCURIE(prefix, name)
+
+# Expressions
+#############
+
+abstract type Expression <: Node end
+
+# Prologue
+##########
+
+abstract type PrologueClause <: Clause end
+
+@auto_hash_equals struct BaseURI <: PrologueClause
+  uri::String
+end
+
+@auto_hash_equals struct Prefix <: PrologueClause
+  name::String
+  uri::String
+end
+
+# Query
+#######
+
+abstract type Query <: Statement end
+
+const VariableBinding = Union{Variable,Pair{Expression,Variable}}
+
+@auto_hash_equals struct Select <: Query
+  variables::Vector{<:VariableBinding}
+  pattern::Vector{Triple}
+  modifiers::Vector{<:Clause}
+  distinct::Bool
+  reduced::Bool
+  
+  function Select(vars::Vector{<:VariableBinding}, pattern::Vector{Triple}, args...;
+                  distinct::Bool=false, reduced::Bool=false)
+    new(vars, pattern, collect(args), distinct, reduced)
+  end
+end
+
+@auto_hash_equals struct Construct <: Query
+  construct::Vector{Triple}
+  pattern::Vector{Triple}
+  modifiers::Vector{<:Clause}
+  
+  function Construct(cons::Vector{Triple}, pattern::Vector{Triple}, args...)
+    new(cons, pattern, collect(args))
+  end
+end
+
+@auto_hash_equals struct Ask <: Query
+  pattern::Vector{Triple}
+  modifiers::Vector{<:Clause}
+  
+  function Ask(pattern::Vector{Triple}, args...)
+    new(pattern, collect(args))
+  end
+end
+
+@auto_hash_equals struct Describe <: Query
+  nodes::Vector{<:Node}
+  pattern::Vector{Triple}
+  modifiers::Vector{<:Clause}
+  
+  function Describe(nodes::Vector{<:Node}, pattern::Vector{Triple}, args...)
+    new(nodes, pattern, collect(args))
+  end
+end
 
 end
